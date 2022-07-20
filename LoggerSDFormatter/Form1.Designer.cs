0e29e15a70a27a8cd020b84d1ac7fe9be642ee98
skilledd007 <65367463+skilledd007@users.noreply.github.com>
2022-07-20 13:58:58 -0600

    Add project files.

---
+++ LoggerSDFormatter/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace LoggerSDFormatter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.FormatSD = new System.Windows.Forms.Button();
+            this.chooseSDCard = new System.Windows.Forms.Button();
+            this.SDFilePath = new System.Windows.Forms.Label();
+            this.StatusMessage = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // FormatSD
+            // 
+            this.FormatSD.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormatSD.Location = new System.Drawing.Point(330, 366);
+            this.FormatSD.Name = "FormatSD";
+            this.FormatSD.Size = new System.Drawing.Size(303, 61);
+            this.FormatSD.TabIndex = 0;
+            this.FormatSD.Text = "FormatSD";
+            this.FormatSD.UseVisualStyleBackColor = true;
+            this.FormatSD.Click += new System.EventHandler(this.FormatSD_Click);
+            // 
+            // chooseSDCard
+            // 
+            this.chooseSDCard.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chooseSDCard.Location = new System.Drawing.Point(330, 183);
+            this.chooseSDCard.Name = "chooseSDCard";
+            this.chooseSDCard.Size = new System.Drawing.Size(303, 63);
+            this.chooseSDCard.TabIndex = 2;
+            this.chooseSDCard.Text = "chooseSDCard";
+            this.chooseSDCard.UseVisualStyleBackColor = true;
+            this.chooseSDCard.Click += new System.EventHandler(this.chooseSDCardButton_Click);
+            // 
+            // SDFilePath
+            // 
+            this.SDFilePath.AutoSize = true;
+            this.SDFilePath.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SDFilePath.Location = new System.Drawing.Point(390, 287);
+            this.SDFilePath.Name = "SDFilePath";
+            this.SDFilePath.Size = new System.Drawing.Size(152, 31);
+            this.SDFilePath.TabIndex = 3;
+            this.SDFilePath.Text = "SDFilePath";
+            // 
+            // StatusMessage
+            // 
+            this.StatusMessage.AutoSize = true;
+            this.StatusMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StatusMessage.Location = new System.Drawing.Point(390, 465);
+            this.StatusMessage.Name = "StatusMessage";
+            this.StatusMessage.Size = new System.Drawing.Size(202, 31);
+            this.StatusMessage.TabIndex = 4;
+            this.StatusMessage.Text = "StatusMessage";
+            this.StatusMessage.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(508, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(485, 62);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "Logger SD Formatter";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::LoggerSDFormatter.Properties.Resources.Screenshot_2022_07_20_134445;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(348, 109);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1016, 679);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.StatusMessage);
+            this.Controls.Add(this.SDFilePath);
+            this.Controls.Add(this.chooseSDCard);
+            this.Controls.Add(this.FormatSD);
+            this.Name = "Form1";
+            this.Text = "LoggerSDFormatter";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button FormatSD;
+        private System.Windows.Forms.Button chooseSDCard;
+        private System.Windows.Forms.Label SDFilePath;
+        private System.Windows.Forms.Label StatusMessage;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ LoggerSDFormatter/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace LoggerSDFormatter
+{
+    public partial class Form1 : Form
+    {
+        string sdFolder = "";
+        public Form1()
+        {
+            InitializeComponent();
+            SDFilePath.Text = "Select SD card";
+        }
+        private void chooseSDCardButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                // shows the path to the selected folder in the folder dialog
+                sdFolder = fbd.SelectedPath;
+            SDFilePath.Text = sdFolder;
+        }
+        private void FormatSD_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo di = new DirectoryInfo(sdFolder);
+            foreach (FileInfo file in di.EnumerateFiles())
+            {
+                file.Delete();
+            }
+            foreach (DirectoryInfo subDirectory in di.EnumerateDirectories())
+            {
+                subDirectory.Delete(true);
+            }
+            di.CreateSubdirectory("batteryinfo");
+            di.CreateSubdirectory("customerinfo");
+            di.CreateSubdirectory("data");
+            di.CreateSubdirectory("logs");
+            di.CreateSubdirectory("settings");
+            di.CreateSubdirectory("tests");
+            di.CreateSubdirectory("webapp");
+            string[] dirs = Directory.GetDirectories(sdFolder, "*", SearchOption.AllDirectories);
+            File.Create(Path.Combine(dirs[0], "batteryinfo.txt"));
+            File.Create(Path.Combine(dirs[1], "customerinfo.txt"));
+            File.Create(Path.Combine(dirs[1], "infofilled.txt"));
+            File.Create(Path.Combine(dirs[1], "installerinfo.txt")); 
+            FileStream settingsFile = File.Create(Path.Combine(dirs[4], "settings.txt"));
+            Byte[] title = new UTF8Encoding(true).GetBytes("Battery Wizard\n");
+            settingsFile.Write(title, 0, title.Length);
+            settingsFile.Close();
+            StatusMessage.Text = "Success!!";
+
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
