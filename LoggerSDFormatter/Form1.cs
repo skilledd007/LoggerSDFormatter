094e36a023567bf297c178babe5793ee73983e33
skilledd007 <65367463+skilledd007@users.noreply.github.com>
2022-07-21 16:24:25 -0600

    Uploading FW Works!

--- LoggerSDFormatter/Form1.cs
+++ LoggerSDFormatter/Form1.cs
@@ -87,11 +87,14 @@ namespace LoggerSDFormatter
 
         }
 
+
         private void upload_Click(object sender, EventArgs e)
         {
             string strCmdText;
-            strCmdText = "/C \"python.exe\" \"esptool.py\" --chip esp32 --port \"" + comboBox1.GetItemText(comboBox1.SelectedItem) + "\" --baud 460800 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 bootloader_dio_40m.bin 0x8000 partitions.bin 0xe000 boot_app0.bin";
+            strCmdText = "/C python -m esptool --chip esp32 --port \"" + comboBox1.GetItemText(comboBox1.SelectedItem) + "\" --baud 460800 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 bootloader_dio_40m.bin 0x8000 partitions.bin 0xe000 boot_app0.bin 0x10000 firmware.bin";
             System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+            
+
         }
     }
 }
